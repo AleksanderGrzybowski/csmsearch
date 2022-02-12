e528240cca811c524e0226ca74a1dc3c8a1e30a3
Aleksander Grzybowski <me@aleksandergrzybowski.pl>
2022-02-12 12:47:10 +0100

    Fix another Polish letter

--- csmsearch/src/App.js
+++ csmsearch/src/App.js
@@ -7,6 +7,7 @@ function normalize(text) {
     text = text.replace(/ó/, "o");
     text = text.replace(/ż/, "z");
     text = text.replace(/ź/, "z");
+    text = text.replace(/Ź/, "z");
     text = text.replace(/ź/, "z");
     text = text.replace(/ł/, "l");
     text = text.replace(/ś/, "s");
